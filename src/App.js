eba610f90fdaecb0d78e7f3441374d7f8ffc2902
Kelvin Alcala Grassal <kalcala@bsc.com.do>
2025-05-22 15:54:57 -0400

    add dockerfile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+const apiUrl = process.env.REACT_APP_API_URL;
+const version = process.env.REACT_APP_VERSION;
+
 
 function App() {
   return (
@@ -7,7 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-			Create by Kalcala v0.1
+			Create by Kalcala
+      <p>Version: {process.env.REACT_APP_VERSION}</p>
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
